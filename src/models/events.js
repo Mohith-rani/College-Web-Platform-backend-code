72eb72a22c7568b35c0db0ef38ac8271c9dacaac
Mohith-rani <135196508+Mohith-rani@users.noreply.github.com>
2024-04-10 13:54:36 +0530

    Add files via upload

---
+++ src/models/events.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema;
+
+const dataSchema = Schema({
+  category:{
+    type:String,
+    required:true
+  },
+  text:{
+   type:String,
+   
+  },
+  dispUrl:{
+    type:String,
+    required:true
+
+  },
+  heading:{
+    type:String
+    
+   },
+  fileType:{
+    type:String,
+    required:true
+  },
+  endDate:{
+    type:String,
+    required:true
+  }
+
+},{timestamps :true})
+
+module.exports = mongoose.model('events',dataSchema);
